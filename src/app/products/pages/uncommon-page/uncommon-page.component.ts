764a489e2ca918ade803f1e78a3218f2e806a131
Fernando de la Cruz Ramos <fcruz@centraldealarmas.com.mx>
2024-04-24 17:50:25 -0600

    uncommon pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+  //i18n Select
+  public name: string = 'Fernando';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
 
+  changeClient(): void{
+    this.name = 'Melissa';
+    this.gender  = 'female';
+  }
+
+
+  //i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia']
 }
